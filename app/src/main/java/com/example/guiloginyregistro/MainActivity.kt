c9b639981acf22979eacd086e8538d49c7cd549c
MrSixtorz <sixto723@gmail.com>
2024-01-17 19:43:52 +0100

    Implementación en  FIrebase, y parte de la autentificación

--- app/src/main/java/com/example/guiloginyregistro/MainActivity.kt
+++ app/src/main/java/com/example/guiloginyregistro/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.guiloginyregistro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Evento personalizado para Google Analytics
+        val analytics : FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message","Integración de mensaje completa")
+        analytics.logEvent("InitScreen", bundle)
     }
 
 
